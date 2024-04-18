c761a40119d4fb92c4cf74ab7815f0a877d86e1e
Paul Gutierrez Velasco <gutierrez.paul@pucp.pe>
2024-04-17 21:43:12 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
@@ -26,6 +27,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+//Lo último
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+}) 
 
 
 app.listen(process.env.PORT, () => {
